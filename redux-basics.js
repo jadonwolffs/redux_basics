a7125b96af7f921c63bdca7bd95ad414655d37da
jadon <jadonwolffs@gmail.com>
2020-02-18 15:21:39 +0200

    initial commit

---
+++ redux-basics.js
@@ -0,0 +1,38 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = { counter: 0 };
+
+//reducer
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+      break;
+    case "ADD_COUNTER":
+    return {
+        ...state,
+        counter: state.counter + action.value
+      };
+      break;
+    default:
+      break;
+  }
+  return state;
+};
+
+//store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//dispatching action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+// store.dispatch({type:'SUB_COUNTER',value:10});
+// store.dispatch({type:'DEC_COUNTER'});
+console.log(store.getState());
+
+//subsription
