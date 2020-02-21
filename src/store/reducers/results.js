cab9d5a0b01c449a7ce5e33c6d86ff45314be4cf
jadon <jadonwolffs@gmail.com>
2020-02-21 11:18:07 +0200

    finished module

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,21 +1,24 @@
-import * as actions from '../actions/actions';
+import * as actions from "../actions/actions";
+import { updateObject } from "../utility";
 const initialState = {
   results: []
+};
+
+const deleteResult = (state, action) => {
+  return { results: state.results.filter(result => result.id !== action.id) };
+};
+const addResult = (state, action) => {
+  return {
+    results: state.results.concat({ id: new Date(), value: action.result })
+  };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actions.STO:
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.result})
-      };
+      return updateObject(state, addResult(state, action));
     case actions.DEL:
-      const newResults = state.results.filter(result => result.id !== action.id);
-      return {
-        ...state,
-        results: newResults
-      };
+      return updateObject(state, deleteResult(state, action));
     default:
       return state;
   }
