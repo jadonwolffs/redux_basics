2999c07f377584da412e7f03d4ca522a74848c2b
jadon <jadonwolffs@gmail.com>
2020-02-21 10:54:11 +0200

    added utility to reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actions from '../actions/actions';
+import {updateObject} from '../utility';
 const initialState = {
   counter: 0
 };
@@ -10,20 +11,15 @@ const reducer = (state = initialState, action) => {
       newState.counter = state.counter + 1;
       return newState;
     case actions.DEC:
-      return {
-        ...state,
-        counter: state.counter - 1
-      };
+      return updateObject(state, {counter:state.counter - 1});
     case actions.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value
-      };
+      return updateObject(state, {counter:state.counter + action.value});
     case actions.SUB:
-      return {
-        ...state,
-        counter: state.counter - action.value
-      };
+      return updateObject(state, {counter:state.counter - action.value});
+      // return {
+      //   ...state,
+      //   counter: state.counter - action.value
+      // };
     default:
       return state;
   }
